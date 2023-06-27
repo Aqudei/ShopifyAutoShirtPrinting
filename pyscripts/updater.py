16e9ad9013b777ebb3de1fccf401482c2bffb313
aqudei <fdeu.aalmine@gmail.com>
2023-06-27 14:44:12 +0800

    aaa

--- pyscripts/updater.py
+++ pyscripts/updater.py
@@ -20,23 +20,27 @@ class Updater:
         shopify.ShopifyResource.activate_session(self.session)
         self.conn = psycopg2.connect(database="thelonelykids", user="postgres",
                                      password="Espelimbergo_122289", host="localhost", port="5432")
-        self.cursor = self.conn.cursor()
-        self.cursor.execute("SET search_path TO public")
+        self.main_cursor = self.conn.cursor()
+        self.main_cursor.execute("SET search_path TO public")
 
     def fetch_existing_lines(self, order_id):
         """
         docstring
         """
         q = 'SELECT * FROM "MyLineItems" WHERE "OrderId"=%s'
-        self.cursor.execute(q, (order_id,))
-        columns = list([c[0] for c in self.cursor.description])
-        items = self.cursor.fetchall()
+        localcursor = self.conn.cursor()
+        localcursor.execute("SET search_path TO public")
+        localcursor.execute(q, (order_id,))
+        columns = list([c[0] for c in localcursor.description])
+        items = localcursor.fetchall()
 
         for item in items:
             item_dict = {}
             for c, i in zip(columns, item):
                 item_dict[c] = i
             yield item_dict['LineItemId'], item_dict
+
+        localcursor.close()
 
     def process_orders(self, orders_response):
         """
@@ -50,18 +54,18 @@ class Updater:
             for line in order.line_items:
                 if not line.id in existing_lines:
                     to_add.append((order, line))
-
+        values = []
         for order_item, line_item in to_add:
             shipping_line = order_item.shipping_lines[0].code
 
-            values = (f"{order_item.order_number}", 0, line_item.sku, line_item.name, line_item.variant_id,
-                      line_item.variant_title, line_item.id, line_item.quantity, 0, line_item.fulfillment_status or '',
-                      order_item.financial_status, f"{order_item.customer.first_name} {order_item.customer.last_name}",
-                      order_item.customer.email, order_item.note or '', order.id,  "Pending", shipping_line)
-            q = """INSERT INTO public."MyLineItems" ("OrderNumber", "BinNumber", "Sku", "Name", "VariantId", "VariantTitle", "LineItemId", "Quantity", "PrintedQuantity","FulfillmentStatus", "FinancialStatus", "Customer", "CustomerEmail", "Notes", "OrderId", "Status", "Shipping") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            print("Parameter values: ", values)
-            self.cursor.execute(q, values)
-            print("Row count: ", self.cursor.rowcount)
+            values.append((f"{order_item.order_number}", 0, line_item.sku, line_item.name, line_item.variant_id,
+                           line_item.variant_title, line_item.id, line_item.quantity, 0, line_item.fulfillment_status or '',
+                           order_item.financial_status, f"{order_item.customer.first_name} {order_item.customer.last_name}",
+                           order_item.customer.email, order_item.note or '', order.id,  "Pending", shipping_line))
+
+        q = """INSERT INTO public."MyLineItems" ("OrderNumber", "BinNumber", "Sku", "Name", "VariantId", "VariantTitle", "LineItemId", "Quantity", "PrintedQuantity","FulfillmentStatus", "FinancialStatus", "Customer", "CustomerEmail", "Notes", "OrderId", "Status", "Shipping") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        self.main_cursor.executemany(q, values)
+        self.conn.commit()
 
     def fetch_orders(self):
         """
@@ -74,8 +78,6 @@ class Updater:
         while (orders_response.has_next_page()):
             orders_response = orders_response.next_page()
             self.process_orders(orders_response)
-        
-        self.conn.commit()
 
     def cleanup(self):
         """
