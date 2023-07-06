645729d02596d2f0d1f97772794bdd4ded24f7c0
aqudei <fdeu.aalmine@gmail.com>
2023-07-06 16:31:05 +0800

    abc

--- pyscripts/tlkapi/admin.py
+++ pyscripts/tlkapi/admin.py
@@ -3,6 +3,16 @@ from .models import Log, OrderInfo, LineItem
 # Register your models here.
 
 
+@admin.register(OrderInfo)
+class OrderInfoAdmin(admin.ModelAdmin):
+    list_display = ['OrderId', 'BinNumber', 'Active']
+
+
+@admin.register(LineItem)
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = ['OrderNumber', 'Name', 'Status']
+
+
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    list_display = ['ChangeDate','ChangeStatus','LineItem']
+    list_display = ['ChangeDate', 'ChangeStatus', 'LineItem']
